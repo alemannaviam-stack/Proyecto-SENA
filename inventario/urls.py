33f7de534a3a9f7ee37ab88f9f634abce2b1124f
Maria <alemannaviam@gmail.com>
2026-07-17 17:34:58 -0500

    app inventario(casi deserto)

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,13 +1,7 @@
 from django.urls import path
-<<<<<<< HEAD
 from . import views
 
 urlpatterns = [
     path('dashboard/', views.dashboard_proveedor, name='dashboard_proveedor'),
     path('agregar/', views.agregar_producto, name='agregar_producto'),
-=======
-
-urlpatterns = [
-    # Si está vacío por ahora, déjalo así con los corchetes
->>>>>>> 0217c3cc69aa399165e2058f6b2025b03c9d4c91
 ]
--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,8 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'usuarios',
     'inventario',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'inventario' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
@@ -118,3 +121,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Al loguearse con éxito, Django los envía a la vista que decide su destino
 LOGIN_REDIRECT_URL = 'redireccionar_por_rol'
 LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = 'login'
